9de1ef01d58d9c07bb0f0fb6d9a9f872d0d135ef
HarryKim24 <tl9434@naver.com>
2024-05-02 15:18:04 +0900

    Create YoutubeAPI Project

---
+++ routes/channels.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const router = express.Router();
+
+router.use(express.json());
+
+let db = new Map();
+var objectId = 1;
+
+router
+  .route('/')
+  .get((req, res) => { // 채널 전체 조회
+    console.log(db);
+
+    var {id} = req.body;
+    var youtubeChannels = [];
+
+    if (db.size && id) {
+      db.forEach(objectChannel => {
+        if (objectChannel.id === id) 
+          youtubeChannels.push(objectChannel);
+      })
+      
+      if (youtubeChannels.length) {
+        res.status(200).json(youtubeChannels);
+      } else {
+        notFoundChannelError();
+      }
+    } else {
+      notFoundChannelError();
+    }
+  })
+  .post((req, res) => { // 채널 개별 생성
+
+    if (req.body.channelTitle) {
+
+      db.set(objectId++, req.body);
+      console.log(db);
+
+      res.status(201).json({
+        message : `${db.get(objectId - 1).channelTitle} 채널이 생성되었습니다!`
+      })
+    } else {
+      res.status(400).json({
+        message : '채널 정보를 다시 입력해주세요.'
+      })
+    }
+  })
+
+router
+  .route('/:id')
+  .get((req, res) => { // 채널 개별 조회
+    console.log(db);
+    let {id} = req.params;
+    id = parseInt(id);
+    var dbChannel = db.get(id);
+
+    if (dbChannel) {
+      res.status(200).json(db.get(id));
+    } else {
+      notFoundChannelError();
+    }
+  })
+  .put((req, res) => { // 채널 개별 수정
+    let {id} = req.params;
+    id = parseInt(id);
+
+    var dbchannel = db.get(id);
+    var oldChannelTitle = dbchannel.channelTitle;
+
+    if (dbchannel) {
+      var newChannelTitle = req.body.channelTitle;
+      dbchannel.channelTitle = newChannelTitle;
+      db.set(id, dbchannel);
+      console.log(db);
+
+      res.status(200).json({
+        message : `채널명이 ${oldChannelTitle}에서 ${newChannelTitle}로 변경되었습니다.`
+      })
+    } else {
+      notFoundChannelError();
+    }
+  }) 
+  .delete((req, res) => { // 채널 개별 삭제
+    console.log(db);
+    let {id} = req.params;
+    id = parseInt(id);
+    var dbChannel = db.get(id);
+
+    if (dbChannel) {
+      db.delete(id);
+      console.log(db);
+      res.status(200).json({
+        message : `${dbChannel.channelTitle} 채널이 삭제되었습니다.`
+      });
+    } else {
+      notFoundChannelError();
+    }
+  }) 
+
+function notFoundChannelError() {
+  res.status(404).json({
+    message : '채널을 찾지 못했습니다.'
+  })
+}
+
+module.exports = router
---
+++ routes/users.js
@@ -0,0 +1,99 @@
+// express 모듈 세팅
+const express = require("express");
+const router = express.Router();
+
+router.use(express.json()); // http 외 모듈 'JOSN' 사용
+
+let db = new Map();
+var objectId = 1; // 객체마다 고유한 ID값 부여
+
+// 로그인 기능
+router.post("/login", (req, res) => {
+  console.log(req.body);
+
+  const {id, password} = req.body;
+  var loginUser = {};
+
+  db.forEach(objectUser => {
+    if (objectUser.id === id) {
+      loginUser = objectUser;
+    }
+  });
+
+  if (isAccount(loginUser)) {
+    if (loginUser.password === password) {
+      res.status(200).json({
+        message : `${loginUser.name}님 로그인 되었습니다.`
+      });
+    } else {
+      res.status(400).json({
+        message : "비밀번호가 틀렸습니다."
+      });
+    }
+  } else {
+    res.status(404).json({
+      message : "존재하지 않는 아이디 입니다."
+    });
+  }
+
+  function isAccount(obj) {
+    if (Object.keys(obj).length === 0) return false;
+    else return true;
+  }
+});
+
+// 회원 가입 기능
+router.post("/join", (req, res) => {
+  if (req.body == {}) {
+    res.status(400).json({
+      message : `회원 정보를 다시 입력해주세요.`,
+    });
+  } else {
+    const {id} = req.body;
+    db.set(id, req.body);
+    console.log(db);
+
+    res.status(201).json({
+      message : `${db.get(id).name}님 반갑습니다!`
+    });
+  }
+});
+
+// 회원 개별 조회
+router.get("/users", (req, res) => {
+  console.log(db);
+  let {id} = req.body;
+  const user = db.get(id);
+
+  if (user) {
+    res.status(200).json({
+      objectId : user.id,
+      objectName : user.name
+    })
+  } else {
+    res.status(404).json({
+      message : "회원 정보가 존재하지 않습니다."
+    })
+  }
+});
+
+// 회원 개별 탈퇴
+router.delete("/users", (req, res) => {
+  let {id} = req.body;
+  const user = db.get(id);
+
+  if (user) {
+    db.delete(id);
+    console.log(db);
+
+    res.status(200).json({
+      message : `${user.name}님 계정이 삭제되었습니다.`
+    })
+  } else {
+    res.status(404).json({
+      message : "회원 정보가 존재하지 않습니다."
+    })
+  }
+});
+
+module.exports = router
